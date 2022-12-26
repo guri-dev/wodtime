dc60472f501fd4b5e1345e02bc3fdc6b6ce61257
gaganphp <mailtogagan990@gmail.com>
2022-12-25 20:41:15 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>Wodtime website</span>
+    </div>
+  );
+}
+
+export default App;
