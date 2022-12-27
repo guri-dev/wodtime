f1efb1224514c1a02ff8de8bab133e052ed38f29
gaganphp <mailtogagan990@gmail.com>
2022-12-27 07:10:38 -0800

    Added top navigations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,10 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header/>
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
       </div>
       <div className="right-h">Right</div>
     </div>
