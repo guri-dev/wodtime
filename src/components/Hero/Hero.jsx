95482b89aa0f387e3f05d226dcd26ed718cf9afc
gaganphp <mailtogagan990@gmail.com>
2022-12-30 16:04:06 -0800

    Working on adding stats after the tagline on home page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,6 +21,23 @@ const Hero = () => {
             </span>            
           </div>
           <div><span>Ideal body</span></div>
+          <div>
+            <span>In here we will help you to shape and build your ideal body and live up your life to the fullest</span>
+          </div>
+        </div>
+        <div className="figures">
+          <div>
+            <span>+ 140</span>
+            <span>Expert coaches</span>
+          </div>
+          <div>
+            <span>+ 978</span>
+            <span> Members joined</span>
+          </div>
+          <div>
+            <span>+ 50</span>
+            <span>Fitness programs</span>
+          </div>
         </div>
       </div>
       <div className="right-h">Right</div>
