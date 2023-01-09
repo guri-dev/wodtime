1ced2028b065b0681e8e3679803a2cf40c8bb719
gaganphp <mailtogagan990@gmail.com>
2023-01-09 12:23:50 -0800

    Working on programs section

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Programs.css';
+const Programs = () => {
+  return (
+    <div className="programs" id="programs">
+        <div className="programs-header">
+            <span>Explore our</span>
+            <span>Program</span>
+            <span>to shape you</span>
+        </div>
+    </div>
+  )
+}
+
+export default Programs
