cddb9fd2a79b0cc3500082e0945b344f8e992534
gaganphp <mailtogagan990@gmail.com>
2023-01-17 08:40:55 -0800

    Working on loading static data into html

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Programs.css';
 import {programsData} from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png';
 const Programs = () => {
   return (
     <div className="Programs" id="programs">
@@ -9,10 +10,13 @@ const Programs = () => {
             <span>Program</span>
             <span className='stroke-text'>to shape you</span>
         </div>
-        <div className="program-categories">okoko
+        <div className="program-categories">
           {programsData.map((program) => (
             <div className="category">
               {program.image}
+              <span>{program.heading}</span>
+              <span>{program.details}</span>
+              <div className="join-now"><span>Join Now</span></div><img src={RightArrow}/>
             </div>
           ))}
         </div>
