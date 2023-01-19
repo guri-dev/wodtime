1c0e0e49d6fc76348515d7e54e0ea946dad99a79
gaganphp <mailtogagan990@gmail.com>
2023-01-19 07:34:55 -0800

    Working on Reasons section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero.jsx';
 import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons.jsx';
 function App() {
   return (
     <div className="App">
           <Hero />
           <Programs />
+          <Reasons />
     </div>
   );
 }
