edc2b150549158c47257c2cbf897926b262b6cd0
gaganphp <mailtogagan990@gmail.com>
2023-01-28 04:39:15 -0800

    Loaded dynamic data in testimonial section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import './Testimonials.css';
+import {testimonialsData} from '../../data/testimonialsData';
+import { useState } from 'react';
+import  leftArrow  from '../../assets/leftArrow.png';
+import  rightArrow  from '../../assets/rightArrow.png';
+const Testimonials = () => {
 
-function Testimonials() {
+  const [selected, setSelected] = useState(0);
+  const tLength = testimonialsData.length;
   return (
     <div className='Testimonials'>
-        <div className='left-t'>
-            <span>Testimonials</span>
-            <span className='stroke-text'>What they</span>
-            <span>say about us</span>
+      <div className="left-t">
+        <span>Testimonial</span>
+        <span className='stroke-text'>what they</span>
+        <span>say about us</span>
+        <span>{testimonialsData[selected].review}</span>
+        <span>
+          <span style={{color: 'var(--orange)'}}>
+            {testimonialsData[selected].name}
+          </span>
+          - {testimonialsData[selected].status}
+        </span>
+      </div>
+      <div className="right-t">
+        <img src={testimonialsData[selected].image} alt="" />
+        <div className="arrows">
+          <img src={leftArrow} alt="" /><img src={rightArrow} alt="" />
         </div>
-        <div className='right-t'>Right</div>
+      </div>
     </div>
   )
 }
