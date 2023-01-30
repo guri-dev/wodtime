8f6b8b32d1f84c98998133541c287c1dee5c86ed
gaganphp <mailtogagan990@gmail.com>
2023-01-30 07:13:05 -0800

    Working on join us section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons.jsx';
 import Plans from './components/Plans/Plans.jsx';
 import Testimonials from './components/Testimonials/Testimonials.jsx';
+import Join from './components/Join/Join.jsx';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
           <Reasons />
           <Plans />
           <Testimonials />
+          <Join />
     </div>
   );
 }
