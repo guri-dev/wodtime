b95aea5ea99d7765d42095fa129336bd96161d05
gaganphp <mailtogagan990@gmail.com>
2023-02-04 07:09:07 -0800

    Adding mobile view

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,17 +4,27 @@ import {testimonialsData} from '../../data/testimonialsData';
 import { useState } from 'react';
 import  leftArrow  from '../../assets/leftArrow.png';
 import  rightArrow  from '../../assets/rightArrow.png';
+import {motion} from 'framer-motion';
 const Testimonials = () => {
 
   const [selected, setSelected] = useState(0);
   const tLength = testimonialsData.length;
+  const transition = { type: "spring", duration: 3}
   return (
     <div className='Testimonials'>
       <div className="left-t">
         <span>Testimonial</span>
         <span className='stroke-text'>what they</span>
         <span>say about us</span>
-        <span>{testimonialsData[selected].review}</span>
+        <motion.span
+          key={selected}
+          initial={{opacity: 0, x: -100}}
+          animate={{opacity: 1, x: 0}}
+          exit={{opacity: 0, x: 100}}
+          transition={transition}
+        >
+          {testimonialsData[selected].review}
+        </motion.span>
         <span>
           <span style={{color: 'var(--orange)'}}>
             {testimonialsData[selected].name}
@@ -23,9 +33,24 @@ const Testimonials = () => {
         </span>
       </div>
       <div className="right-t">
-        <div></div>
-        <div></div>
-        <img 
+        <motion.div
+          inherit={{opacity: 0, x: -100}}
+          transition={{ ...transition, duration: 2}}
+          whileInView={{ opacity: 1, x: 0}}
+        >
+        </motion.div>
+        <motion.div
+          inherit={{opacity: 0, x: 100}}
+          transition={{ ...transition, duration: 2}}
+          whileInView={{ opacity: 1, x: 0}}
+        >
+        </motion.div>
+        <motion.img 
+          key={selected}
+          initial={{opacity: 0, x: 100}}
+          animate={{opacity: 1, x: 0}}
+          exit={{opacity: 0, x: -100}}
+          transition={transition}
           src={testimonialsData[selected].image}           
           alt="" />
         <div className="arrows">
